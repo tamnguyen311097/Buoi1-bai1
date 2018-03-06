69094039e634223cca3db89677942482a5ed0c83
USER <USER@ASUS>
2018-03-06 23:59:07 +0700

    Add project files.

---
+++ Bai1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+
+namespace Bai1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string strHostName = Dns.GetHostName();
+            Console.WriteLine("Host name: " + strHostName);
+
+            //Find host by name
+            IPHostEntry iphostentry = Dns.GetHostByName(strHostName);
+
+            // Enumerate IP address
+            int nIP = 0;
+            foreach (IPAddress ipaddress in iphostentry.AddressList)
+                Console.WriteLine("IP " + ++nIP + ":" + ipaddress.ToString());
+        }
+    }
+}
